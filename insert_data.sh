72a6475f69582fe93aa4ed145a4416b82187da9b
Fabrizio Bruno <34265667+fabri95@users.noreply.github.com>
2023-07-12 10:47:46 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -34,3 +34,5 @@ do
     OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'");
     $($PSQL "insert into games(year , round , winner_id , opponent_id , winner_goals , opponent_goals)  values ($YEAR , '$ROUND' , $WINNER_ID , $OPPONENT_ID , $WINNER_GOALS , $OPPONENT_GOALS)")
   fi
+
+done
