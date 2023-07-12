285f1e79a3135782f3b6cae887c4380c2425459b
Fabrizio Bruno <34265667+fabri95@users.noreply.github.com>
2023-07-12 11:04:29 +0200

    Updated insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -21,18 +21,18 @@ do
     # if the values are not found
     if [[ -z $WINNER_ID ]]
     then 
-      $($PSQL "insert into teams(name) values ('$WINNER')")
+      echo $($PSQL "insert into teams(name) values ('$WINNER')")
      # WINNER_ID = $($PSQL "select team_id from teams where team=$WINNER")
     fi
     if [[ -z $OPPONENT_ID ]]
     then 
-      $($PSQL "insert into teams(name) values ('$OPPONENT')")
+      echo $($PSQL "insert into teams(name) values ('$OPPONENT')")
      # OPPONENT_ID = $($PSQL "select team_id from teams where team=$OPPONENT")
 
     fi
     WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'");
     OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'");
-    $($PSQL "insert into games(year , round , winner_id , opponent_id , winner_goals , opponent_goals)  values ($YEAR , '$ROUND' , $WINNER_ID , $OPPONENT_ID , $WINNER_GOALS , $OPPONENT_GOALS)")
+    echo $($PSQL "insert into games(year , round , winner_id , opponent_id , winner_goals , opponent_goals)  values ($YEAR , '$ROUND' , $WINNER_ID , $OPPONENT_ID , $WINNER_GOALS , $OPPONENT_GOALS)")
   fi
 
 done
